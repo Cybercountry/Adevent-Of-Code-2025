d19a4d3bee4f631933dcd53b958a45566f025072
Matheus <matheus@dukcab.com>
2025-12-01 23:48:37 -0300

    ADD: Day-1 Exercise part 2

--- Day-1/main.py
+++ Day-1/main.py
@@ -6,27 +6,44 @@ with open("input.txt") as f:
 # Variables
 dial_position = 50
 zero_hits = 0
+zero_crossings = 0
 
 # Process commands
 for item in data:
-    spin_direction = item[0]
-    value = int(item[1:])
+    dial_direction = item[0]
+    magnitude = int(item[1:])
 
-    # According to the problem statement,
-    # if we encounter the letter 'R',
-    # it indicates we should perform an addition operation by turning the dial to the right.
-    if spin_direction == "R":
-        dial_position = (dial_position + value) % 100
-    # If it’s not the letter R, then it will be the letter 'L', and in this case,
-    # we do the opposite—a subtraction by turning the dial to the left.
+    # Check whether the first 't' pass through 0 occurs at each rotation
+    if dial_direction == "R":
+        # Form: t = (100 - x) % 100
+        t = (100 - dial_position) % 100
+        # Special case: if t = 0, so the real first pass is at t = 100
+        if t == 0:
+            t = 100
     else:
-        dial_position = (dial_position - value) % 100
-    # The problem states that numbers range from 0 to 99,
-    # so we need to handle circular counting, which we can achieve using modulo,
-    # 100 in this case, since that’s the total number of elements we have.
+        # Form: t = x
+        t = dial_position
+        # Same situation where the dial starts at 0, in this case t = 100
+        if t == 0:
+            t = 100
 
-    # Count how many times the dial returns to 0
+    # Check the first pass happens within the rotation range [0, magnitude)
+    if t < magnitude:
+        # 1 occurrence or more
+        zero_crossings += 1
+
+        # Calculate the maximum number of occurrences within the magnitude
+        k_max = (magnitude - 1 - t) // 100
+        zero_crossings += k_max
+
+    # Update the dial position applying the direction
+    if dial_direction == "R":
+        dial_position = (dial_position + magnitude) % 100
+    else:  # 'L'
+        dial_position = (dial_position - magnitude) % 100
+
+    # Count if the rotation ends exactly at 0
     if dial_position == 0:
         zero_hits += 1
 
-print(zero_hits)
+print(zero_hits + zero_crossings)
