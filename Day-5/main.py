73c04f31b49a752b3d9aadade5b2e9864fad8f58
Matheus <matheus@dukcab.com>
2025-12-05 15:34:26 -0300

    ADD: Day-5 Exercise part 2

--- Day-5/main.py
+++ Day-5/main.py
@@ -1,35 +1,49 @@
-def read_ranges_ids(filename="input.txt", mode="r"):
+def read_ranges(filename="input.txt"):
     ranges = []
-    id_availables = []
-    with open(filename, mode) as file:
-        read_range = True
-
+    with open(filename) as file:
         for row in file:
             row = row.strip()
 
-            # Check for the empty line
-            # Change the mode of reading
             if row == "":
-                read_range = False
-                continue
+                break
 
-            if read_range:
-                x, y = row.split("-")
-                ranges.append((int(x), int(y)))
-            else:
-                id_availables.append(int(row))
-    return ranges, id_availables
+            start, end = map(int, row.split("-"))
+            ranges.append((int(start), int(end)))
+
+    return ranges
 
 
-# O(n * m)
+# O(n log n)
+# Memory: O(n)
 def main():
-    ranges, id_availables = read_ranges_ids()
+    # Sorted Example = [(10, 14), (11, 20)]
+    sorted_ranges = sorted(read_ranges(), key=lambda x: x[0])
 
-    fresh_available = 0
-    for id in id_availables:
-        if any(start <= id <= end for start, end in ranges):
-            fresh_available += 1
-    print(fresh_available)
+    total = 0
+    current_start, current_end = sorted_ranges[0]
+
+    """
+    The overlap condition: start <= current_end + 1
+    [10, 14]
+    start, end = [11, 20] -> 11 <= 14+1 (15) -> True
+    Ranges:     [10---14]
+                      [11---------20]
+    Result:
+    [10------------------20]
+    """
+
+    for start, end in sorted_ranges[1:]:
+        if start <= current_end + 1:
+            # Overlap or adjacent -> merge, extend current range
+            current_end = max(current_end, end)
+        else:
+            # If no overlap, sum the finished range and reset current range
+            total += current_end - current_start + 1
+            current_start, current_end = start, end
+
+    # Add the last range to the total
+    total += current_end - current_start + 1
+    print(total)
 
 
 if __name__ == "__main__":
